5f41d2c56d84d6aec994032b2b8188df5a59a569
JulienC_W97 <julien.cauquis@gmail.com>
2025-11-30 21:39:55 +0100

    updated navbar link

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         <div className={`fixed inset-x-0 z-20 w-full ${!isOpen ? 'backdrop-blur-lg bg-primary/40' : ''}`}>
             <div className="mx-auto c-space max-w-7xl">
                 <div className="flex items-center justify-between py-2 sm:py-0">
-                    <a href="/" className="text_xl font-bold transition-colors text-neutral-400 hover:text-white">
+                    <a href="/portfolio/" className="text_xl font-bold transition-colors text-neutral-400 hover:text-white">
                         Julien
                     </a>
                     <button onClick={() => setIsOpen(!isOpen)} className="flex cursor-pointer text-neutral-400 hover:text-white focus:outline-none sm:hidden">
