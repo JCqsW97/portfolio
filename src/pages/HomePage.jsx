747f9266b5d35ea3c405ceacab5f53dd46992067
JulienC_W97 <julien.cauquis@gmail.com>
2025-11-30 22:03:27 +0100

    add HeroText

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import Hero from "../sections/Hero";
+
 function HomePage() {
   return (
     <div>
         <h1 className="flex justify-center">Thinking outside the box!</h1>
+        <Hero />
     </div>
   )
 };
