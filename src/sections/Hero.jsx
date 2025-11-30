8aae99e49caa63a36d3f3c7e8660a8b54fa41551
JulienC_W97 <julien.cauquis@gmail.com>
2025-11-30 22:33:01 +0100

    add Astronaut + all for Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,16 @@
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+import { easing } from "maath";
+import { Float } from "@react-three/drei";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import { Astronaut } from "../components/Astronaut";
 
 const Hero = () => {
+
+    const isMobile = useMediaQuery({ maxWidth: 853 });
 
     return (
         <section 
@@ -10,8 +19,36 @@ const Hero = () => {
         >
             <HeroText />
             <ParallaxBackground />
+            <figure 
+                className="absolute inset-0"
+                style={{ width: "100vw", height: "100vh" }}
+                >
+                <Canvas camera={{ position: [0, 1, 3] }}>
+                    <Suspense fallback={<Loader />}>
+                        <Float>
+                            <Astronaut 
+                                scale={isMobile && 0.23}
+                                position={isMobile && [0, -1.5, 0]} 
+                            />
+                        </Float>
+                    </Suspense>
+                    <Rig />
+                </Canvas>
+            </figure>
         </section>
     );
+};
+
+function Rig() {
+    return useFrame((state, delta) => {
+        easing.damp3(
+            state.camera.position,
+            [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+            0.5,
+            delta
+        );
+    });
+
 };
 
 export default Hero;
