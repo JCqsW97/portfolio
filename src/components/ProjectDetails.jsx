e10839fca7fa8147488ad7944e1c8e7cf7a9045d
JulienC_W97 <julien.cauquis@gmail.com>
2025-12-01 10:39:39 +0100

    add Projects section and related components

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,63 @@
+import { motion } from "motion/react";
+import { useEffect } from "react";
+
+const ProjectDetails = ({
+    title, 
+    description, 
+    subDescription, 
+    href, 
+    image, 
+    tags,
+    closeModal,
+}) => {
+    useEffect(() => {
+        // Prevent background scroll
+        document.body.classList.add('overflow-hidden');
+        // Cleanup on unmount or close
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+    }, []);
+    
+    return (
+        <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full backdrop-blur-sm">
+            <motion.div 
+                className="relative max-w-2xl border shadow-sm rounded-2xl bg-gradient-to-l from-midnight to-navy border-white/10 max-h-full overflow-y-auto overflow-x-auto"
+                initial={{ opacity:0, scale:0.5 }}
+                animate={{ opacity:1, scale:1 }}
+            >
+                <button 
+                    onClick={closeModal} 
+                    className="sticky top-5 left-[calc(100%-3.75rem)] p-2 rounded-sm bg-midnight hover:bg-gray-500 z-100"
+                >
+                    <img src="/portfolio/assets/close.svg" className="w-6 h-6" alt="close button" />
+                </button>
+                <img src={image} alt={title} className="w-full rounded-t-2xl relative -top-11" />
+                <div className="p-5">
+                    <h5 className="mb-2 text-2xl font-bold text-white">{title}</h5>
+                    <p className="mb-3 font-normal text-neutral-400">{description}</p>
+                    {subDescription.map((subDesc, index) => (
+                        <p key={index} className="mb-3 font-normal text-neutral-400">{subDesc}</p>
+                        ))}
+                    <div className="flex items-center justify-between mt-11">
+                        <div className="flex gap-3"> 
+                            {tags.map((tag) => (
+                                <img 
+                                    key={tag.id} 
+                                    src={tag.path} 
+                                    alt={tag.name} 
+                                    className="rounded-lg size-10 hover-animation"
+                                />
+                            ))}
+                        </div>
+                        <a className="inline-flex items-center gap-1 font-medium cursor-pointer hover-animation" href={href} target="_blank" rel="noopener noreferrer">
+                            View Project <img src="/portfolio/assets/arrow-up.svg" className="size-4" alt="arrow up icon" />
+                        </a>
+                    </div>
+                </div>
+            </motion.div>
+        </div>
+    );
+};
+
+export default ProjectDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import Hero from "../sections/Hero";
 import About from "../sections/About";
+import Projects from "../sections/Projects";
 
 function HomePage() {
   return (
@@ -7,6 +8,7 @@ function HomePage() {
         <h1 className="flex justify-center">Thinking outside the box!</h1>
         <Hero />
         <About />
+        <Projects />
     </div>
   )
 };
