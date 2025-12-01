dbda548336c4befffb162a08143688e15f9195c9
JulienC_W97 <julien.cauquis@gmail.com>
2025-12-01 10:29:32 +0100

    Add About section and associated components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import Hero from "../sections/Hero";
+import About from "../sections/About";
 
 function HomePage() {
   return (
     <div>
         <h1 className="flex justify-center">Thinking outside the box!</h1>
         <Hero />
+        <About />
     </div>
   )
 };
